788206eee12eb9f8b328a7b33a05ed3d8ffa5163
Mrnutt131 <nutt28484@gmail.com>
2026-06-23 21:36:13 +0700

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ import './index.js';
 import './botwelcome.js';
 import './bothelper.js';
 import './guardian.js';
-import './guardianadminlog.js';
 
 
 console.log("🌊 LomLayRak Ecosystem Started Successfully!");
