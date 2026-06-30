fac001724a8cb83f26996063eb19d4cc43bf8c09
Mrnutt131 <nutt28484@gmail.com>
2026-06-30 13:43:53 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-console.log("🚀 กำลังเริ่มต้นระบบบอท Lomlayrak ทั้ง 3 ตัวบน Railway...");
+console.log("🚀 กำลังเริ่มต้นระบบบอท Lomlayrak ทั้ง 4 ตัวบน Railway...");
 
+// นำเข้าไฟล์บอททั้งหมด
 import './index.js';
 import './botwelcome.js';
 import './bothelper.js';
 import './guardian.js';
-import './botReaction.js';
-
+import './botReaction.js'; // เพิ่มบอท Reaction เข้าไปตรงนี้ครับ
 
 console.log("🌊 LomLayRak Ecosystem Started Successfully!");
